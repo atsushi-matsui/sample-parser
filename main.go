210521e68abb5f51237c73b194b54e4f5b4f9fde
atsushi-matsui <kiburedo0702@gmail.com>
2024-05-04 23:57:36 +0900

    clean main

--- main.go
+++ main.go
@@ -13,48 +13,15 @@ import (
 
 func main() {
 	input, _ := antlr.NewFileStream(os.Args[1])
-	//lexer := parsing.NewSearchQueryLexer(input)
 	lexer := parsing.NewDefaultOrSearchQueryLexer(input)
 	stream := antlr.NewCommonTokenStream(lexer, 0)
 
-	//p := parsing.NewSearchQueryParser(stream)
 	p := parsing.NewDefaultOrSearchQueryParser(stream)
 	p.AddErrorListener(antlr.NewDiagnosticErrorListener(true))
-	//listener := parser.NewTreeShapeListener()
 	listener := parser.NewDefaultOrListener()
 	antlr.ParseTreeWalkerDefault.Walk(listener, p.AndExpression())
 
 	res := listener.PopQuery()
 	resJSON, _ := json.Marshal(res)
 	fmt.Printf("res: %s\n", resJSON)
-
-	//if p.GetError() != nil {
-	//	fmt.Println(p.GetError())
-	//} else {
-	//	// p := &parsing.BaseSearchQueryVisitor{}
-	//	tree := p.Expr()
-	//
-	//	visitor := parser.NewTreeShapeVisitor()
-	//
-	//	//visitor.Visit(tree)
-	//	tree.Accept(visitor)
-	//	fmt.Println(tree.GetChild(0).(antlr.ParseTree).GetText()) // Convert the result to string using GetText()
-	//	fmt.Println(tree.GetChild(0).GetChild(0).(antlr.ParseTree).GetText())
-	//	//fmt.Println(tree.GetChild(1))
-	//	//fmt.Println(tree.GetChild(2).(antlr.ParseTree).GetText())
-	//	//fmt.Println(tree.GetChild(2).GetChild(0).(antlr.ParseTree).GetText())
-	//	//fmt.Println(tree.GetChild(2).GetChild(0).GetChild(0).(antlr.ParseTree).GetText())
-	//	//fmt.Println(tree.GetChild(2).GetChild(0).GetChild(0).GetChild(0).(antlr.ParseTree).GetText())
-	//	//fmt.Println(tree.GetChild(2).GetChild(0).GetChild(0).GetChild(1).(antlr.ParseTree).GetText())
-	//	//fmt.Println(tree.GetChild(2).GetChild(0).GetChild(0).GetChild(2).(antlr.ParseTree).GetText())
-	//	//fmt.Println(tree.GetChild(2).GetChild(0).GetChild(0).GetChild(1).GetChild(0).(antlr.ParseTree).GetText())
-	//	//fmt.Println(tree.GetChild(2).GetChild(0).GetChild(0).GetChild(1).GetChild(1).(antlr.ParseTree).GetText())
-	//	//fmt.Println(tree.GetChild(2).GetChild(0).GetChild(0).GetChild(1).GetChild(2).(antlr.ParseTree).GetText())
-	//	//fmt.Println(tree.GetChild(2).GetChild(0).GetChild(0).GetChild(1).GetChild(2).GetChild(0).GetChild(0).GetChild(0).(antlr.ParseTree).GetText())
-	//	//fmt.Println(tree.GetChild(2).GetChild(0).GetChild(0).GetChild(1).GetChild(2).GetChild(0).GetChild(0).GetChild(1).(antlr.ParseTree).GetText())
-	//	//fmt.Println(tree.GetChild(2).GetChild(0).GetChild(0).GetChild(1).GetChild(2).GetChild(0).GetChild(0).GetChild(2).(antlr.ParseTree).GetText())
-	//	//fmt.Println(tree.GetChild(2).GetChild(0).GetChild(0).GetChild(1).GetChild(2).GetChild(0).GetChild(0).GetChild(1).GetChild(0).GetChild(0).(antlr.ParseTree).GetText())
-	//	//fmt.Println(tree.GetChild(2).GetChild(0).GetChild(0).GetChild(1).GetChild(2).GetChild(0).GetChild(0).GetChild(1).GetChild(0).GetChild(1).(antlr.ParseTree).GetText())
-	//	//fmt.Println(tree.GetChild(2).GetChild(0).GetChild(0).GetChild(1).GetChild(2).GetChild(0).GetChild(0).GetChild(1).GetChild(0).GetChild(2).(antlr.ParseTree).GetText())
-	//}
 }
